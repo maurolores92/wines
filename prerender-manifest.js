65e8677f2984bb4b60d44b8214d06e016b70bf60
Mauricio Lores <maurolores1992@gmail.com>
2023-09-27 20:48:05 -0300

    Updates

---
+++ prerender-manifest.js
@@ -0,0 +1,1 @@
+self.__PRERENDER_MANIFEST="{\"version\":4,\"routes\":{},\"dynamicRoutes\":{},\"preview\":{\"previewModeId\":\"d2cfb6ee140a10da24efc7c28bc55b02\",\"previewModeSigningKey\":\"60308bf7b4a427d742cdebef09182d4e2646a9b1a9537a8d71f7f2ff9806419c\",\"previewModeEncryptionKey\":\"92c312f1ca9f6eed6b677181fe97a2c17137ec74fab227f422ed8f61ddfe50cf\"},\"notFoundRoutes\":[]}"
