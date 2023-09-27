f02193180585d8644f0aab897df01a995b6c30f0
Mauricio Lores <maurolores1992@gmail.com>
2023-09-27 20:11:58 -0300

    agregando _app

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+// _app.tsx
+import { AppProps } from 'next/app';
+import '../styles/global.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
